0335cad01a286447ddc0a6004d21eaf741f5172f
sebastien.berruer <sebastien.berruer@adventiel.fr>
2020-01-20 16:11:39 +0100

    add test for querying and specify order

--- src/test/java/com/example/demo/repository/BookRepositoryIT.java
+++ src/test/java/com/example/demo/repository/BookRepositoryIT.java
@@ -2,22 +2,35 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Book;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BookRepositoryIT {
 
     @Autowired
     private BookRepository repository;
 
     @Test
-    public void testCreate() {
+    @Order(1)
+    public void testSave() {
         Book book = new Book();
 
         repository.save(book);
 
         Assertions.assertThat(book.getId()).isNotNull();
+    }
+
+    @Test
+    @Order(2)
+    public void testFindAll() {
+        Iterable<Book> books = repository.findAll();
+
+        Assertions.assertThat(books).hasSizeGreaterThanOrEqualTo(1);
     }
 }
